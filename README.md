a7b0e7e6cea7ce8a07dd98952539ff942776b071
Mohnish Thallavajhula <mohnish@billfloat.com>
2013-01-20 14:05:00 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## hi5
+
+hi5 is a simple script that allows users to hi5 a web page. It doesn't need any sort of registration from it's users.
+
+### Setup
+
+- Create a firebase account and setup a firebase API endpoint. (TODO: detailed explanation)
+- Update the reference in the code. (TODO: detailed explanation)
+- Include the required files. (TODO: detailed explanation)
+
+
+## Screenshots
+
+TODO
+
+
+Inspired by the kudos feature of [Svbtle](http://svbtle.com).
+
+## <3
+
+Built with Firebase and lots of [<3](http://twitter.com/arrowgunz)!
+
+## License
+
+MIT
---
+++ hi5.js
@@ -0,0 +1,98 @@
+(function(window, document) {
+  var element = document.querySelector('#hi5')
+    , feedback = document.querySelector('.feedback')
+    , hi5ed = false
+    , db = new Firebase('https://hi5.firebaseio.com')
+    , timerId = null
+    , id
+    , url
+    , value = {}
+    , count
+    , isNew;
+  window.hi5 = {}
+
+  hi5.sanitizedUrl = function(hashed) {
+    if (hashed) {
+      return md5(document.location.protocol + '//' + document.location.host + document.location.pathname);
+    } else {
+      return document.location.protocol + '//' + document.location.host + document.location.pathname;
+    }
+  };
+
+  hi5.handleHi5 = function() {
+    hi5.hideFeedback();
+    hi5.updateCount();
+    hi5.setCookie();
+    hi5.removeEvents();
+    hi5.changeFeedbackText('Thanks!');
+    hi5.showFeedback();
+  };
+
+  hi5.updateCount = function() {
+    value[id] = {url: url, count: ++count};
+    if (isNew) {
+      db.set(value);
+    } else {
+      db.child(id).update({count: count});
+    }
+  };
+
+  hi5.setCookie = function() {
+    cookie.set(id, true);
+  };
+
+  hi5.hideFeedback = function() {
+    feedback.className = feedback.className.replace(/hi5-show/g, 'hi5-hide');
+  };
+
+  hi5.showFeedback = function() {
+    feedback.className = feedback.className.replace(/hi5-hide/g, 'hi5-show');
+  };
+
+  hi5.changeFeedbackText = function(text) {
+    feedback.innerHTML = text;
+  };
+
+  hi5.onMouseOver = function(e) {
+    hi5.showFeedback();
+    timerId = window.setTimeout(hi5.handleHi5, 1000);
+  };
+
+  hi5.onMouseOut = function(e) {
+    window.clearTimeout(timerId);
+    hi5.hideFeedback();
+  };
+
+  hi5.attachEvents = function() {
+    element.addEventListener('mouseover', hi5.onMouseOver);
+    element.addEventListener('mouseout', hi5.onMouseOut);
+  };
+
+  hi5.removeEvents = function() {
+    element.removeEventListener('mouseover', hi5.onMouseOver);
+    element.removeEventListener('mouseout', hi5.onMouseOut);
+  };
+
+  hi5.setup = function() {
+    id = hi5.sanitizedUrl(true);
+    url = hi5.sanitizedUrl();
+
+    db.child(id).on('value', function(snapshot) {
+      var retrievedValue = snapshot.val() && snapshot.val().count;
+      if (retrievedValue) {
+        count =  retrievedValue;
+        isNew = false;
+      } else {
+        count = 0;
+        isNew = true;
+      }
+      element.innerHTML = count;
+    });
+
+    if ( "true" != cookie.get(id)) {
+      hi5.attachEvents();
+    }
+  };
+
+  hi5.setup();
+})(window, document);
